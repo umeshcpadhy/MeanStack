3d6196d6dc522ab4a1bb0a24c871ad2653981d8c
umeshcpadhy <umesh.padhy@cognizant.com>
2017-06-13 07:30:09 +0000

    adding files

--- server.js
+++ server.js
@@ -3,9 +3,38 @@ var bodyParser = require('body-parser');
 var mongoose=require('mongoose');
 var app = express();
 app.use(bodyParser.json());
+app.use(express.static('public')); 
 mongoose.connect('mongodb://localhost:27017/mittens');
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+var Meow = mongoose.model('Meow', {
+ text: String
+  });
+
+app.get('/meows', function(req,res,next){
+  Meow.find({}, function(err, meows){
+    return res.json(meows);
+  });
+});
+
+app.post('/meows', function(req, res, next){	
+		var newMeow = new Meow({
+			text: req.body.newMeow		
+		});
+		newMeow.save(function(err){
+			return res.send("Added Successfully");
+	});	
+});
+
+app.put('/meows/remove', function(req, res, next){	
+var meowId=req.body.meow._id;		
+		Meow.remove({_id: meowId}, function(err){
+			return res.send("Deleted Successfully");
+	});
+});
+
+
+
+app.listen(4000, function () {
+  console.log('Example app listening on port 4000!')
 });
 
